25a95fe3bbe735c0b2f5b1e40b258e7dce425544
Yanmife123 <yanmifeigwe@gmail.com>
2024-05-24 09:53:10 +0100

    created the sign-up section

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 (function () {
+  let message = {
+    error_message: '"let this input be up to 7 character"',
+  };
   /* for login page required */
   function required() {
-    document.querySelectorAll(".input").forEach((input) => {
+    document.querySelectorAll(".login-input").forEach((input) => {
       if (input.value.length < 7) {
-        input.classList.add("required");
-        setTimeout(() => {
-          document.getElementById("Username").remove &&
-            document.getElementById("password").remove;
-          input.classList.remove("required");
-        }, 3000);
+        let ID = input.id;
+        let label = document.getElementById(ID + "1");
+        if (display == 0) {
+          input.classList.add("required");
+          label.innerHTML = message.error_message;
+          label.classList.add("label-appear");
+          setTimeout(() => {
+            document.getElementById("Username").remove &&
+              document.getElementById("password").remove;
+            input.classList.remove("required");
+          }, 3000);
+        } else {
+          document
+        }
       }
     });
   }
@@ -18,27 +29,37 @@
   /* for the animation of login to sign up */
   let display = 0;
   let animation = document.querySelector("#animation-box");
-  function header_transition1() {
+  function sign_click() {
     if (display == 0) {
       animation.classList.add("click-fromRight");
       animation.classList.remove("click-fromLeft");
       display = 1;
+      document.querySelectorAll(".hidden-show").forEach((input) => {
+        input.classList.toggle("hidden");
+        input.classList.toggle("sign-up");
+      });
+      document.querySelectorAll("label").forEach((label) => {
+        label.innerHTML = "";
+      });
     }
-
     console.log(display);
   }
 
-  function header_transition2() {
+  function login_click() {
     if (display == 1) {
       animation.classList.add("click-fromLeft");
       animation.classList.remove("click-fromRight");
       display = 0;
+      document.querySelectorAll(".hidden-show").forEach((input) => {
+        input.classList.toggle("hidden");
+        input.classList.toggle("sign-up");
+      });
     }
 
     console.log(display);
   }
   let login = document.querySelector("#login");
   let signUP = document.querySelector("#sign");
-  signUP.addEventListener("click", header_transition1);
-  login.addEventListener("click", header_transition2);
+  signUP.addEventListener("click", sign_click);
+  login.addEventListener("click", login_click);
 })();
